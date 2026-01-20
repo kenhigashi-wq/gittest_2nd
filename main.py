c5040debd47b9d6b7149288b7e789bab765e6b72
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-01-20 17:36:50 +0000

    Pull request

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #Hols the entire user interface
 
 def main():
-    pass
+    choice = input("Select 1 to create a character and 2 ro exit")
+
+    if choice == "1":
+        pass
+    else:
+        pass
 
 main()
